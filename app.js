eb04c9b25ec65b04eaaf1d50d8d8f1f3635087f5
Vinicius <viniciusmoura.vmg@uni9.edu.br>
2026-05-22 18:06:05 -0300

    Atualização do sistema estante virtual

--- app.js
+++ app.js
@@ -8,35 +8,147 @@ const rl = readline.createInterface({
 
 function iniciarSistema() {
 
-    console.log("=== ESTANTE VIRTUAL ===");
-    console.log("1 - Listar livros");
-    console.log("2 - Cadastrar livro");
+  console.log("\n=== ESTANTE VIRTUAL ===");
+  console.log("1 - Listar livros");
+  console.log("2 - Cadastrar livro");
+  console.log("3 - Buscar livro");
+  console.log("4 - Remover livro");
+  console.log("5 - Editar livro");
+  console.log("6 - Vender livro");
+  console.log("0 - Sair");
 
-    rl.question("Escolha uma opção: ", function(resposta) {
+  rl.question("Escolha uma opção: ", function(resposta) {
 
-        if (resposta == 1) {
-            console.log(funcoes.listarLivros());
+    if (resposta == 1) {
 
-            iniciarSistema();
-        }
+      funcoes.listarLivros();
 
-        if (resposta == 2) {
+      iniciarSistema();
+    }
 
-            rl.question("Nome do livro: ", function(nome) {
+    else if (resposta == 2) {
 
-                rl.question("Autor do livro: ", function(autor) {
+      rl.question("Nome do livro: ", function(nome) {
 
-                    console.log(
-                        funcoes.cadastrarLivro(nome, autor)
-                    );
+        rl.question("Autor do livro: ", function(autor) {
 
-                    iniciarSistema();
+          rl.question("Preço do livro: ", function(preco) {
 
-                });
+            rl.question("Quantidade em estoque: ", function(estoque) {
+
+              console.log(
+                funcoes.cadastrarLivro(
+                  nome,
+                  autor,
+                  Number(preco),
+                  Number(estoque)
+                )
+              );
+
+              iniciarSistema();
+
             });
-        }
 
-    });
+          });
+
+        });
+
+      });
+
+    }
+
+    else if (resposta == 3) {
+
+      rl.question("Digite o nome do livro: ", function(nome) {
+
+        console.log(
+          funcoes.buscarLivro(nome)
+        );
+
+        iniciarSistema();
+
+      });
+
+    }
+
+    else if (resposta == 4) {
+
+      rl.question("Digite o ID do livro: ", function(id) {
+
+        console.log(
+          funcoes.removerLivro(Number(id))
+        );
+
+        iniciarSistema();
+
+      });
+
+    }
+
+    else if (resposta == 5) {
+
+      rl.question("Digite o ID do livro: ", function(id) {
+
+        rl.question("Novo nome: ", function(nome) {
+
+          rl.question("Novo autor: ", function(autor) {
+
+            rl.question("Novo preço: ", function(preco) {
+
+              rl.question("Novo estoque: ", function(estoque) {
+
+                console.log(
+                  funcoes.editarLivro(
+                    Number(id),
+                    nome,
+                    autor,
+                    Number(preco),
+                    Number(estoque)
+                  )
+                );
+
+                iniciarSistema();
+
+              });
+
+            });
+
+          });
+
+        });
+
+      });
+
+    }
+
+    else if (resposta == 6) {
+
+      rl.question("Digite o ID do livro: ", function(id) {
+
+        console.log(
+          funcoes.venderLivro(Number(id))
+        );
+
+        iniciarSistema();
+
+      });
+
+    }
+
+    else if (resposta == 0) {
+
+      console.log("Sistema encerrado.");
+      rl.close();
+    }
+
+    else {
+
+      console.log("Opção inválida.");
+
+      iniciarSistema();
+    }
+
+  });
 
 }
 
--- funcoes/livrosFuncoes.js
+++ funcoes/livrosFuncoes.js
@@ -2,13 +2,153 @@ const livros = require("../dados/livros");
 const fs = require("fs");
 
 function salvarLivros() {
-    fs.writeFileSync(
-        "./dados/livros.json",
-        JSON.stringify(livros, null, 2)
-    );
+  fs.writeFileSync(
+    "./dados/livros.json",
+    JSON.stringify(livros, null, 2)
+  );
 }
+
 function listarLivros() {
-  return livros;
+
+  if (livros.length === 0) {
+    return "Nenhum livro cadastrado.";
+  }
+
+  livros.forEach(livro => {
+
+    console.log(`
+========================
+ID: ${livro.id}
+Nome: ${livro.nome}
+Autor: ${livro.autor}
+Preço: R$ ${livro.preco}
+Estoque: ${livro.estoque}
+Disponível: ${livro.disponivel ? "Sim" : "Não"}
+========================
+`);
+  });
+
+}
+
+function cadastrarLivro(nome, autor, preco, estoque) {
+
+  if (!nome || !autor) {
+    return "Nome e autor são obrigatórios.";
+  }
+
+  if (preco < 0 || estoque < 0) {
+    return "Preço e estoque não podem ser negativos.";
+  }
+
+  const novoId = livros.length > 0
+    ? livros[livros.length - 1].id + 1
+    : 1;
+
+  const novoLivro = {
+    id: novoId,
+    nome,
+    autor,
+    preco,
+    estoque,
+    disponivel: estoque > 0
+  };
+
+  livros.push(novoLivro);
+
+  salvarLivros();
+
+  return "Livro cadastrado com sucesso.";
+}
+
+function buscarLivro(nome) {
+
+  let livrosEncontrados = livros.filter(
+    l => l.nome.toLowerCase().includes(nome.toLowerCase())
+  );
+
+  if (livrosEncontrados.length === 0) {
+    return "Livro não encontrado.";
+  }
+
+  livrosEncontrados.forEach(livro => {
+
+    console.log(`
+========================
+ID: ${livro.id}
+Nome: ${livro.nome}
+Autor: ${livro.autor}
+Preço: R$ ${livro.preco}
+Estoque: ${livro.estoque}
+Disponível: ${livro.disponivel ? "Sim" : "Não"}
+========================
+`);
+
+  });
+
+}
+
+
+function removerLivro(id) {
+
+  let indiceLivro = livros.findIndex(
+    l => l.id === id
+  );
+
+  if (indiceLivro === -1) {
+    return "Livro não encontrado.";
+  }
+
+  livros.splice(indiceLivro, 1);
+
+  salvarLivros();
+
+  return "Livro removido com sucesso.";
+}
+
+function editarLivro(id, novoNome, novoAutor, novoPreco, novoEstoque) {
+
+  let livro = livros.find(
+    l => l.id === id
+  );
+
+  if (!livro) {
+    return "Livro não encontrado.";
+  }
+
+  livro.nome = novoNome;
+  livro.autor = novoAutor;
+  livro.preco = novoPreco;
+  livro.estoque = novoEstoque;
+  livro.disponivel = novoEstoque > 0;
+
+  salvarLivros();
+
+  return "Livro atualizado com sucesso.";
+}
+
+function venderLivro(id) {
+
+  let livro = livros.find(
+    l => l.id === id
+  );
+
+  if (!livro) {
+    return "Livro não encontrado.";
+  }
+
+  if (livro.estoque <= 0) {
+    return "Livro sem estoque.";
+  }
+
+  livro.estoque--;
+
+  if (livro.estoque === 0) {
+    livro.disponivel = false;
+  }
+
+  salvarLivros();
+
+  return "Venda realizada com sucesso.";
 }
 
 module.exports = {
@@ -16,54 +156,6 @@ module.exports = {
   cadastrarLivro,
   buscarLivro,
   removerLivro,
-  editarLivro
+  editarLivro,
+  venderLivro
 };
-function cadastrarLivro(nome, autor) {
-  const novoLivro = {
-    id: livros.length + 1,
-    nome: nome,
-    autor: autor,
-    disponivel: true
-  };
-
-  livros.push(novoLivro);
-
-  salvarLivros();
-
-  return "Livro cadastrado com sucesso";
-}
-function buscarLivro(nome){
-
-    let livro = livros.find(l => l.nome === nome);
-
-    if (!livro) {
-        return "Livro não encontrado";
-    }
-
-    return livro;
-}
-
-function removerLivro(id) {
-  let indiceLivro = livros.findIndex(l => l.id === id);
-
-  if (indiceLivro === -1) {
-    return "Livro não encontrado";
-  }
-
-  livros.splice(indiceLivro, 1);
-
-salvarLivros();
-
-  return "Livro removido com sucesso";
-}
-function editarLivro(nomeAtual, novoNome){
-    let livro = livros.find(l => l.nome === nomeAtual)
-    if (!livro){
-        return "Livro não cadastrado."
-    }
-    livro.nome = novoNome; 
-
-    salvarLivros();
-
-    return "Livro atualizado com sucesso"
-}
